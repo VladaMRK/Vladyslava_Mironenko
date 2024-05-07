f9469918e541bc3c73f02ab71585feb904da78e7
Mironenko <vladislava_mironenko@gmail.com>
2024-05-07 15:17:54 +0200

    25

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def has_33(list_of_integers: list) -> bool:
 
 
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
